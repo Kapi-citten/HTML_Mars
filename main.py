39e224b26884be9dcb642ea65e4d97e14e78741a
Kapi <michaelpo88@mail.ru>
2025-03-20 17:11:27 +0300

    Advertising with an image

---
+++ main.py
@@ -0,0 +1,101 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_html():
+    return '<h1>Миссия Колонизация Марса</h1>'
+
+
+@app.route('/index')
+def index():
+    return '<h1>И на Марсе будут яблони цвести!</h1>'
+
+
+@app.route('/promotion_image')
+def promote():
+    return f'''<!doctype html>
+    <html lang="en">
+        <head>
+            <title>Колонизация</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='styles/style.css')}">
+        </head>
+        <body>
+            <h1>Жди нас, Марс!</h1>
+            
+            <img src=https://static.tildacdn.com/tild3432-3939-4763-b237-393664633666/1.png 
+            alt="здесь должна была быть картинка, но не нашлась">
+            
+            <h2 class=black>Человечеству мала одна планета.</h2>
+            <h2 class="green">Человечество вырастает из детства.</h2>
+            <h2 class="grey">Мы сделаем обитаемыми безжизненные пока планеты.</h2>
+            <h2 class="yellow">И начнем с Марса!</h2>
+            <h2 class="red">Присоединяйся!</h2>
+        </body>
+    </html>
+    '''
+
+
+@app.route('/foo')
+def foo():
+    names = ['Аня', 'Маша', 'Есения', 'Дина', 'Алина', 'Инга']
+    s = ''
+    for i, name in enumerate(names):
+        s += f'<h{i + 1}>{name}</h{i + 1}>'
+    return s
+
+
+@app.route('/image_mars')
+def image():
+    return f''' <title>Привет, Марс!</title>
+    <h1>Жди нас, Марс!</h1>
+    <img src=https://static.tildacdn.com/tild3432-3939-4763-b237-393664633666/1.png 
+    alt="здесь должна была быть картинка, но не нашлась">
+    <p>Вот она такая, красная планета.</p>'''
+
+
+@app.route('/css')
+def css_sample():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='styles/style.css')}" />
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Первая HTML-страница</h1>
+                  </body>
+                </html>"""
+
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Яндекс!</h1>
+                    <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                    </div>
+                  </body>
+                </html>'''
+
+@app.route('/')
+def login():
+    return
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 8080)
